39665d8b06f6048a8d157869a6906f05577b4169
s.111chikiii <s.111chikiii@gmail.com>
2014-10-18 16:51:10 +0900

    add username

---
+++ app/models/user.rb
@@ -0,0 +1,33 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :authentication_keys => [:username]
+
+  #usernameを必須とする
+  validates_uniqueness_of :username
+  validates_presence_of :username
+  # validates :username, presence: true, uniqueness: true
+
+  #usernameを利用してログインするようにオーバーライド
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      #認証の条件式を変更する
+      where(conditions).where(["username = :value", { :value => username }]).first
+    else
+      where(conditions).first
+    end
+  end
+
+  #emailを不要とする
+  def email_required?
+    false
+  end
+
+  def email_changed?
+    false
+  end
+
+end
