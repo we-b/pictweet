7cfb8a87ca841d0e84db5c6217b6b10855a945cd
Yuji Goto <yuji1129xxx@gmail.com>
2014-10-29 10:49:40 +0900

    fix

---
+++ config/unicorn.rb
@@ -0,0 +1,34 @@
+# ワーカーの数
+worker_processes 4
+
+# RAILS_ROOT を指定
+working_directory "/var/www/html/log/current"
+
+# ソケット
+listen File.expand_path('tmp/sockets/unicorn.sock', ENV['RAILS_ROOT'])
+# pid
+pid File.expand_path('tmp/pids/unicorn.pid', ENV['RAILS_ROOT'])
+
+# ログ
+stderr_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
+stdout_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
+
+# ダウンタイムなくす
+preload_app true
+
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+
+  old_pid = "#{ server.config[:pid] }.oldbin"
+  unless old_pid == server.pid
+    begin
+      # SIGTTOU だと worker_processes が多いときおかしい気がする
+      Process.kill :QUIT, File.read(old_pid).to_i
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
+  end
+end
+
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+end
