f58cb6a3469a85cd8e3721ca4fae21304e584a2f
Yuji Goto <yuji1129xxx@gmail.com>
2014-11-06 11:17:09 +0900

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :authentication_keys => [:email]
+         :recoverable, :rememberable, :trackable, :validatable
   has_many :tweets
 
   validates_uniqueness_of :username
