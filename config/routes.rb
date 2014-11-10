70b96c18ca7522daf9c51b80ed7f5bf5b3898493
Yuji Goto <yuji1129xxx@gmail.com>
2014-11-11 00:08:54 +0900

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,9 @@ Rails.application.routes.draw do
   # get     '/tweets'           => 'tweets#index'
   # post    '/tweets'           => 'tweets#create'
   # get     '/tweets/new'       => 'tweets#new'
+  # delete  '/tweets/:id'       => 'tweets#destroy'
   # get     '/tweets/:id/edit'  => 'tweets#edit'
   # patch   '/tweets/:id'       => 'tweets#update'
-  # delete  '/tweets/:id'       => 'tweets#destroy'
 
   resources :users, :only => [:show]
   # get     '/users/:id'        => 'users#show'
